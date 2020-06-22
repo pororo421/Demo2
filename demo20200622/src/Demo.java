d8ed9550a3e2af352534ded29c01b13958197cc5
pororo421 <40535642+pororo421@users.noreply.github.com>
2020-06-22 23:30:02 +0900

    Update Demo.java

--- demo20200622/src/Demo.java
+++ demo20200622/src/Demo.java
@@ -1,6 +1,12 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(125);
+         System.out.println(12310);
+        System.out.println(12320);
+        System.out.println(12330);
+        System.out.println(12340);
+        System.out.println(12350);
+        System.out.println(12360);
+        System.out.println(12370);
     }
 }
