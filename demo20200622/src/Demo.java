b4cf918d8445c4030fad9dd3b2ca474d1646185d
RG <396390583@qq.com>
2020-06-22 23:30:39 +0900

    修改

--- demo20200622/src/Demo.java
+++ demo20200622/src/Demo.java
@@ -1,6 +1,12 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(123);
+        System.out.println(1231);
+        System.out.println(1232);
+        System.out.println(1233);
+        System.out.println(1234);
+        System.out.println(1235);
+        System.out.println(1236);
+        System.out.println(1237);
     }
 }
