d835382b7bd2c8de123bfd2b9ee0f48811d002f6
RG <396390583@qq.com>
2020-06-23 00:06:56 +0900

    修改

--- demo20200622/src/Demo.java
+++ demo20200622/src/Demo.java
@@ -1,13 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(1231);
-        System.out.println(1232);
-        System.out.println(1233);
-        System.out.println(1234);
-        System.out.println(1235);
-        System.out.println(1236);
-        System.out.println(1237);
+
     }
     public String getName(String a){
         return a+"123";
